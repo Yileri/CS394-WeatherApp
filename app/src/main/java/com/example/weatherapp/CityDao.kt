ba34af8dc71fe289dc840ee1d3d809405a3e85b3
Ekin Gundogdu <ekin.gundogdu@ozu.edu.tr>
2023-01-11 20:00:34 +0300

    CityDao

--- app/src/main/java/com/example/weatherapp/CityDao.kt
+++ app/src/main/java/com/example/weatherapp/CityDao.kt
@@ -1,8 +1,12 @@
 package com.example.weatherapp
 
 import androidx.room.Dao
+import androidx.room.Query
+import com.example.weatherapp.model.City
 
 @Dao
 interface CityDao {
+    @Query("SELECT* FROM cities")
+    fun getAll():List<City>
 
 }
