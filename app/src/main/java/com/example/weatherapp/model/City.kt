7d93ea57b4d451336f2c0707c616419bacfa26b8
Ekin Gundogdu <ekin.gundogdu@ozu.edu.tr>
2023-01-11 19:20:02 +0300

    Room requirements

--- app/src/main/java/com/example/weatherapp/model/City.kt
+++ app/src/main/java/com/example/weatherapp/model/City.kt
@@ -1,5 +1,16 @@
 package com.example.weatherapp.model
 
-data class City(val name: String, val condition: String, val temp: String
-, val highTemp: String, val lowTemp: String, val sunrise: String
-, val sunset: String, val humidity: String, val wind: String) : java.io.Serializable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName="cities")
+data class City(@PrimaryKey val name: String,
+                @ColumnInfo(name="condition") val condition: String,
+                @ColumnInfo(name="temp")val temp: String,
+                @ColumnInfo(name="highTemp")val highTemp: String,
+                @ColumnInfo(name="lowTemp")val lowTemp: String,
+                @ColumnInfo(name="sunrise") val sunrise: String,
+                @ColumnInfo(name="sunset")val sunset: String,
+                @ColumnInfo(name="humidity") val humidity: String,
+                @ColumnInfo(name="wind")val wind: String) : java.io.Serializable
