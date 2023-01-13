33851fc77736bfb30bf8c48e71a077d07ac49109
Ekin Gundogdu <ekin.gundogdu@ozu.edu.tr>
2023-01-13 14:00:13 +0300

    api

--- app/src/main/java/com/example/weatherapp/retrofit/ApiInterface.kt
+++ app/src/main/java/com/example/weatherapp/retrofit/ApiInterface.kt
@@ -4,9 +4,8 @@ package com.example.weatherapp.retrofit
 import com.example.weatherapp.model.CityX
 import retrofit2.http.GET
 
-val CityExample:String="London"// burda hangi şehirleri çekizec yazmak yapmak lazım
 
-val BASE_URL:String="https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=001f4abe47262aa2424f9aed04fa56c1"// çekeceğimiz adress bve benim sebaımla girdiğim API key
+
 
 interface ApiInterface {
     @GET("data/2.5/weather?q=London&units=metric&appid=001f4abe47262aa2424f9aed04fa56c1")
